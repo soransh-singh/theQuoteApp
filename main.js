f79a0a20ddb666fbd0dae42f4dad0d6359c7fcdc
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-06-30 00:17:44 +0530

    update css

--- main.js
+++ main.js
@@ -3,21 +3,20 @@ window.addEventListener("load", ()=>{
   const randomBtn = document.querySelector(".random-btn")
   const quote = document.querySelector(".quote")
   const name = document.querySelector(".name")
+  const genre = document.querySelector(".genre")
+
+  const apiUrl = "https://quote-garden.herokuapp.com/api/v3/quotes/random"
 
   async function getQuote(){
-    const response = await fetch('https://quote-garden.herokuapp.com/api/v3/quotes/random')
+    const response = await fetch(apiUrl)
     const responseData = await response.json()
-    const {quoteText, quoteAuthor} = responseData.data[0]
+    const {quoteText, quoteAuthor, quoteGenre} = responseData.data[0]
     quote.textContent = quoteText
-    name.textContent = quoteAuthor
-    console.log(responseData.data[0]);
-    console.log(quoteText);
-    console.log(quoteAuthor);
+    name.textContent = `- by ${quoteAuthor}`
+    genre.textContent = quoteGenre
   }
 
   randomBtn.addEventListener("click", getQuote)
 
   getQuote()
-
-
 })
